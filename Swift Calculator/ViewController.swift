096fcde7f6e2f1624933a9b86653d419db4543d3
Caner Ates <cnrates@gmail.com>
2019-09-07 15:58:51 +0800

    Initial Commit

---
+++ Swift Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Calculator
+//
+//  Created by Django on 2019/9/7.
+//  Copyright © 2019 Caner Ates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
