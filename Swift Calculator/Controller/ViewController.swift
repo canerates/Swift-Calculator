b4f3e667d527510b5fafe425d8d77612dc0caf29
Caner Ates <cnrates@gmail.com>
2019-09-07 19:35:17 +0800

    Implemented auto layout design

--- Swift Calculator/Controller/ViewController.swift
+++ Swift Calculator/Controller/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
 
